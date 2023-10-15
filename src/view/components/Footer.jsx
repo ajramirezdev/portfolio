d9522e3afbbfbdf2f3f7e00f0d7c2389d853e69d
ae_jay <ajlimramirez@gmail.com>
2023-10-15 14:16:17 +0800

    added project descriptions

--- src/view/components/Footer.jsx
+++ src/view/components/Footer.jsx
@@ -3,10 +3,10 @@ import { LiaGitlab } from "react-icons/lia";
 
 const Footer = () => {
   return (
-    <section className="flex justify-center h-44 bg-neutral-950 text-neutral-300">
+    <section className="flex justify-center py-24 md:py-28 bg-neutral-950 text-neutral-300">
       <div className="flex items-center text-base gap-3">
         <a
-          className="font-bold cursor-pointer hover:text-neutral-400 transition ease-out duration-500"
+          className="font-bold cursor-pointer sm:text-lg md:text-xl xl:text-2xl hover:text-neutral-400 transition ease-out duration-500"
           href="#top"
         >
           ajramirez.dev
@@ -17,7 +17,7 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <FaLinkedin />
+          <FaLinkedin size={20} />
         </a>
         <a
           href="https://github.com/ajramirezdev"
@@ -25,7 +25,7 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <FaGithub />
+          <FaGithub size={20} />
         </a>
         <a
           href="https://gitlab.com/ae_jay"
@@ -33,7 +33,7 @@ const Footer = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <LiaGitlab />
+          <LiaGitlab size={20} />
         </a>
       </div>
     </section>
--- src/view/components/Projects.jsx
+++ src/view/components/Projects.jsx
@@ -1,7 +1,18 @@
 import chirp from "../../assets/chirp.png";
 import d2tt from "../../assets/d2tt.png";
 import ramble from "../../assets/ramble.png";
-import tlpkmn from "../../assets/tlpkmn.png";
+import tlpkmn from "../../assets/tlpkmn.jpg";
+import portfolio from "../../assets/portfolio.jpg";
+import HTML from "../../assets/html.png";
+import CSS from "../../assets/css.png";
+import JS from "../../assets/js.png";
+import React from "../../assets/react.png";
+import Tailwind from "../../assets/tailwind.png";
+import NodeJs from "../../assets/nodejs.png";
+import MongoDB from "../../assets/mongodb.svg";
+import Express from "../../assets/express.png";
+import SocketIO from "../../assets/socketio.png";
+import Firebase from "../../assets/firebase.png";
 
 const Projects = () => {
   return (
@@ -18,15 +29,60 @@ const Projects = () => {
               alt="ramble chat app screenshot"
             />
             <div className="project-content flex flex-col justify-between absolute top-0 left-0 w-full h-full p-5 bg-neutral-100">
-              <div>
+              <div className="flex flex-col sm:gap-3">
                 <p className=" text-neutral-800 text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold m-0">
                   Ramble chat app
                 </p>
+                <div className="flex gap-2">
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Tailwind}
+                    alt="Tailwind CSS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={React}
+                    alt="React icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={NodeJs}
+                    alt="NodeJS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Express}
+                    alt="Express icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={MongoDB}
+                    alt="MongoDB icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={SocketIO}
+                    alt="Socket.io icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Firebase}
+                    alt="Firebase icon"
+                  />
+                </div>
               </div>
               <div>
                 <p className="text-sm sm:text-base md:text-lg xl:text-xl text-neutral-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ramble is a webiste created to meet new people and connect you
+                  with your friends.
+                  <span className="hidden sm:inline">
+                    {" "}
+                    It empowers you to establish chatrooms for your groups or
+                    exchange private messages. To enable real-time chatting, we
+                    harnessed the capabilities of Socket.io, and we incorporated
+                    Firebase Storage, allowing users to effortlessly upload and
+                    display their profile pictures.
+                  </span>
                 </p>
               </div>
               <div className="flex text-sm gap-2 ">
@@ -34,7 +90,7 @@ const Projects = () => {
                   href="https://ramble-murex.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800"
+                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800 hover:bg-neutral-700 hover:border-neutral-700 transition ease-out duration-500"
                 >
                   live demo
                 </a>
@@ -42,7 +98,7 @@ const Projects = () => {
                   href="https://github.com/ajramirezdev/ramble-be"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 border-neutral-800 rounded"
+                  className="border-1 px-2 border-neutral-800 rounded hover:border-neutral-700 hover:text-neutral-700 transition ease-out duration-500"
                 >
                   source code
                 </a>
@@ -57,15 +113,43 @@ const Projects = () => {
               alt="dota 2 tournament tracker screenshot"
             />
             <div className="project-content flex flex-col justify-between absolute top-0 left-0 w-full h-full p-5 bg-neutral-100">
-              <div>
+              <div className="flex flex-col sm:gap-3">
                 <p className=" text-neutral-800 text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold m-0">
                   dota2tournamenttracker
                 </p>
+                <div className="flex gap-2">
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={React}
+                    alt="React icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={NodeJs}
+                    alt="NodeJS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Express}
+                    alt="Express icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={MongoDB}
+                    alt="MongoDB icon"
+                  />
+                </div>
               </div>
               <div>
                 <p className="text-sm sm:text-base md:text-lg xl:text-xl text-neutral-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Using the data I fetched from OpenDota API, I made a webiste
+                  that operates like a search engine for Dota 2 tournaments.
+                  <span className="hidden sm:inline">
+                    When users pick a specific tournament, they are directed to
+                    a page with more details, including the teams involved and
+                    recent match information. Users can also log in and bookmark
+                    their preferred tournaments.
+                  </span>
                 </p>
               </div>
               <div className="flex text-sm gap-2 ">
@@ -73,7 +157,7 @@ const Projects = () => {
                   href="https://dota2tournamenttracker.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800"
+                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800 hover:bg-neutral-700 hover:border-neutral-700 transition ease-out duration-500"
                 >
                   live demo
                 </a>
@@ -81,7 +165,7 @@ const Projects = () => {
                   href="https://github.com/ajramirezdev/dota2tournamenttracker"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 border-neutral-800 rounded"
+                  className="border-1 px-2 border-neutral-800 rounded hover:border-neutral-700 hover:text-neutral-700 transition ease-out duration-500"
                 >
                   source code
                 </a>
@@ -96,15 +180,38 @@ const Projects = () => {
               alt="talpokemon game"
             />
             <div className="project-content flex flex-col justify-between absolute top-0 left-0 w-full h-full p-5 bg-neutral-100">
-              <div>
+              <div className="flex flex-col sm:gap-3">
                 <p className=" text-neutral-800 text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold m-0">
                   TaLPoKeMoN
                 </p>
+                <div className="flex gap-2">
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={CSS}
+                    alt="CSS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={JS}
+                    alt="JavaScript icon"
+                  />
+                </div>
               </div>
               <div>
                 <p className="text-sm sm:text-base md:text-lg xl:text-xl text-neutral-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  The concept for this JavaScript game came from a playful twist
+                  on the words "Pokemon" and "Talpakan," which I found funny.
+                  <span className="hidden sm:inline">
+                    This led me to create a turn-based game, reminiscent of the
+                    Pokemon series, where players engage in battles not with
+                    Pokémons but with chickens. I hope you enjoy this
+                    Frankenstein of a game!
+                  </span>
                 </p>
               </div>
               <div className="flex text-sm gap-2 ">
@@ -112,7 +219,7 @@ const Projects = () => {
                   href="https://talpokemon.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800"
+                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800 hover:bg-neutral-700 hover:border-neutral-700 transition ease-out duration-500"
                 >
                   live demo
                 </a>
@@ -120,7 +227,7 @@ const Projects = () => {
                   href="https://github.com/ajramirezdev/talpokemon"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 border-neutral-800 rounded"
+                  className="border-1 px-2 border-neutral-800 rounded hover:border-neutral-700 hover:text-neutral-700 transition ease-out duration-500"
                 >
                   source code
                 </a>
@@ -135,15 +242,35 @@ const Projects = () => {
               alt="chirp landing page"
             />
             <div className="project-content flex flex-col justify-between absolute top-0 left-0 w-full h-full p-5 bg-neutral-100">
-              <div>
+              <div className="flex flex-col sm:gap-3">
                 <p className=" text-neutral-800 text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold m-0">
                   Chirp.
                 </p>
+                <div className="flex gap-2">
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={HTML}
+                    alt="HTML icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={CSS}
+                    alt="CSS icon"
+                  />
+                </div>
               </div>
               <div>
                 <p className="text-sm sm:text-base md:text-lg xl:text-xl text-neutral-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  I put together this landing page for Chirp using HTML and CSS,
+                  mainly to practice laying out content and working with media
+                  queries.
+                  <span className="hidden sm:inline">
+                    {" "}
+                    What's interesting about this project is that it was the
+                    first time I focused on mobile view styling. I quickly
+                    realized that starting with mobile design made easier for me
+                    to make the website more responsive and user-friendly.
+                  </span>
                 </p>
               </div>
               <div className="flex text-sm gap-2 ">
@@ -151,7 +278,7 @@ const Projects = () => {
                   href="https://ajramirezdev.github.io/chirp-landing-page/"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800"
+                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800 hover:bg-neutral-700 hover:border-neutral-700 transition ease-out duration-500"
                 >
                   live demo
                 </a>
@@ -159,7 +286,7 @@ const Projects = () => {
                   href="https://github.com/ajramirezdev/chirp-landing-page"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 border-neutral-800 rounded"
+                  className="border-1 px-2 border-neutral-800 rounded hover:border-neutral-700 hover:text-neutral-700 transition ease-out duration-500"
                 >
                   source code
                 </a>
@@ -167,38 +294,71 @@ const Projects = () => {
             </div>
           </div>
 
-          <div className="project flex items-center justify-center mx-auto relative aspect-video bg-neutral-200 rounded-lg overflow-hidden">
+          <div className="project dark-shadow flex items-center justify-center mx-auto relative aspect-video bg-neutral-200 rounded-lg overflow-hidden">
             <img
               className="project-image w-full h-full object-contain"
-              src={chirp}
+              src={portfolio}
               alt="chirp landing page"
             />
             <div className="project-content flex flex-col justify-between absolute top-0 left-0 w-full h-full p-5 bg-neutral-100">
-              <div>
+              <div className="flex flex-col sm:gap-3">
                 <p className=" text-neutral-800 text-xl sm:text-2xl md:text-3xl xl:text-4xl font-bold m-0">
-                  Chirp.
+                  Portfolio
                 </p>
+                <div className="flex gap-2">
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Tailwind}
+                    alt="HTML icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={React}
+                    alt="CSS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={NodeJs}
+                    alt="CSS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={Express}
+                    alt="CSS icon"
+                  />
+                  <img
+                    className="w-5 h-5 sm:w-6 sm:h-6 md:w-7 md:h-7 object-contain"
+                    src={MongoDB}
+                    alt="CSS icon"
+                  />
+                </div>
               </div>
               <div>
                 <p className="text-sm sm:text-base md:text-lg xl:text-xl text-neutral-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Using what I've learned so far, I expressed myself creatively
+                  in making my latest portfolio.
+                  <span className="hidden sm:inline">
+                    {" "}
+                    This website serves as a canvas for me to express my
+                    abilities and showcase my most recent projects, providing
+                    visitors with a comprehensive look at what I have to offer.
+                  </span>
                 </p>
               </div>
               <div className="flex text-sm gap-2 ">
                 <a
-                  href="https://ajramirezdev.github.io/chirp-landing-page/"
+                  href="https://ajramirez-portfolio.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800"
+                  className="border-1 px-2 bg-neutral-800 rounded text-neutral-200 border-neutral-800 hover:bg-neutral-700 hover:border-neutral-700 transition ease-out duration-500"
                 >
                   live demo
                 </a>
                 <a
-                  href="https://github.com/ajramirezdev/chirp-landing-page"
+                  href="https://github.com/ajramirezdev/portfolio"
                   target="_blank"
                   rel="noreferrer"
-                  className="border-1 px-2 border-neutral-800 rounded"
+                  className="border-1 px-2 border-neutral-800 rounded hover:border-neutral-700 hover:text-neutral-700 transition ease-out duration-500"
                 >
                   source code
                 </a>
